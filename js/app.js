f36a46491d4ea2e8bae501568220a1cb541ccf70
solixdev <solixdev1@gmail.com>
2024-05-15 10:50:50 +0330

    Defined variables of DOM

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const insertInput = document.getElementById('itemInput')
+const addButton = document.getElementById('addButton')
+const clearButton = document.getElementById('clearButton')
+const todoContainer = document.getElementById('todoList')
+
